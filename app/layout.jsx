e2804549f9e1fbdbd78bd137ff916ca4850efc67
izoh-creatives <izohcreatives@gmail.com>
2024-07-25 10:57:17 +0300

    Added opengraph image

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,6 +6,7 @@ import Footer from "@/app/components/common/Footer";
 import { ToastContainer } from "react-toastify";
 
 export const metadata = {
+  metadataBase: new URL("https://www.izohcreatives.co.ke"),
   title: "Izoh Creatives",
   description:
     "Hey ,I am Izoh.I create modern websites using the latest technologies like React and Next Js.With as little as Ksh 10,000 you get a professional website.Wherever you are.",
