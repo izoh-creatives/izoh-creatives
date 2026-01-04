6d5e15c792c6ae349cbba6b8103e391ecb0d9ab4
izoh-yegon <izohthegreat@gmail.com>
2026-01-04 22:09:13 +0300

    Updated projects

--- app/components/projects.jsx
+++ app/components/projects.jsx
@@ -20,44 +20,46 @@ const Projects = () => {
   }, [activeCategory]);
 
   return (
-    <div
-      id="projects"
-      className="flex flex-col space-y-12 pl-3 pr-7 py-8 lg:py-16 sm:pl-1 sm:pr-4 lg:px-8"
-    >
-      {/* Heading and buttons */}
-      <div className="flex flex-col items-center">
-        {/* Heading */}
-        <div className="flex flex-col items-center space-y-2 mb-8">
+    activeProjects.length > 0 && (
+      <div
+        id="projects"
+        className="flex flex-col space-y-12 pl-3 pr-7 py-8 lg:py-16 sm:pl-1 sm:pr-4 lg:px-8"
+      >
+        {/* Heading and buttons */}
+        <div className="flex flex-col items-center">
           {/* Heading */}
-          <SectionHeading text="Latest Projects" />
-        </div>
-        {/* Filter buttons */}
-        <div className="flex items-center space-x-5">
-          <CategoryButton
-            text="All"
-            isActive={activeCategory == ""}
-            onClick={() => setCategory("")}
-          />
-          {projectCategories.map((category, index) => (
+          <div className="flex flex-col items-center space-y-2 mb-8">
+            {/* Heading */}
+            <SectionHeading text="Latest Projects" />
+          </div>
+          {/* Filter buttons */}
+          <div className="flex items-center space-x-5">
             <CategoryButton
-              key={index}
-              text={category.name}
-              isActive={activeCategory == category.slug}
-              onClick={() => setCategory(category.slug)}
+              text="All"
+              isActive={activeCategory == ""}
+              onClick={() => setCategory("")}
             />
-          ))}
+            {projectCategories.map((category, index) => (
+              <CategoryButton
+                key={index}
+                text={category.name}
+                isActive={activeCategory == category.slug}
+                onClick={() => setCategory(category.slug)}
+              />
+            ))}
+          </div>
+        </div>
+        {/* Projects */}
+        <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4  sm:gap-x-2 md:gap-x-2 lg:gap-x-7 lg:gap-y-20 px-1.5">
+          {activeProjects
+            .filter((project, index) => index <= 7)
+            .map((project, index) => (
+              // project
+              <ProjectCard key={index} project={project} />
+            ))}
         </div>
       </div>
-      {/* Projects */}
-      <div className="grid grid-cols-1 gap-y-12 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4  sm:gap-x-2 md:gap-x-2 lg:gap-x-7 lg:gap-y-20 px-1.5">
-        {activeProjects
-          .filter((project, index) => index <= 7)
-          .map((project, index) => (
-            // project
-            <ProjectCard key={index} project={project} />
-          ))}
-      </div>
-    </div>
+    )
   );
 };
 
